7fd0a9e86a931b318f0cd8aa6b86295014863441
Maydiv123 <maydiv.docs@gmail.com>
2025-08-27 15:00:58 +0530

    Update pipelineRoutes.js

--- node_backend/routes/pipelineRoutes.js
+++ node_backend/routes/pipelineRoutes.js
@@ -12,6 +12,7 @@ router.get('/jobs', pipelineController.getAllJobs);
 router.get('/myjobs', pipelineController.getMyJobs);
 router.get('/jobs/:id', pipelineController.getJobById);
 router.post('/jobs', pipelineController.createJob);
+router.put('/jobs/:id', pipelineController.updateJob);
 router.post('/jobs/:id/stage2', pipelineController.updateStage2);
 router.post('/jobs/:id/stage3', pipelineController.updateStage3);
 router.post('/jobs/:id/stage4', pipelineController.updateStage4);
