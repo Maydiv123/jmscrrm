72b892a0025f6de81e6a97e1d3f40d9bc7b8dbdd
Maydiv123 <maydiv.docs@gmail.com>
2025-08-27 14:50:00 +0530

    Update index.js

--- node_backend/models/index.js
+++ node_backend/models/index.js
@@ -23,6 +23,7 @@ const sequelize = new Sequelize(
 const UserModel = require('./User');
 const PipelineJobModel = require('./PipelineJob');
 const Stage1DataModel = require('./Stage1Data');
+const Stage1ContainerModel = require('./Stage1Container');
 const Stage2DataModel = require('./Stage2Data');
 const Stage3DataModel = require('./Stage3Data');
 const Stage3ContainerModel = require('./Stage3Container');
@@ -37,6 +38,7 @@ const TaskUpdateModel = require('./TaskUpdate');
 const User = UserModel(sequelize);
 const PipelineJob = PipelineJobModel(sequelize);
 const Stage1Data = Stage1DataModel(sequelize);
+const Stage1Container = Stage1ContainerModel(sequelize);
 const Stage2Data = Stage2DataModel(sequelize);
 const Stage3Data = Stage3DataModel(sequelize);
 const Stage3Container = Stage3ContainerModel(sequelize);
@@ -47,9 +49,13 @@ const Task = TaskModel(sequelize);
 const TaskAssignment = TaskAssignmentModel(sequelize);
 const TaskUpdate = TaskUpdateModel(sequelize);
 
-// Define model associations
+// Define associations - CORRECTED based on Golang structure
 PipelineJob.hasOne(Stage1Data, { foreignKey: 'job_id', as: 'Stage1' });
 Stage1Data.belongsTo(PipelineJob, { foreignKey: 'job_id' });
+
+// Stage1Container association
+PipelineJob.hasMany(Stage1Container, { foreignKey: 'job_id', as: 'Stage1Containers' });
+Stage1Container.belongsTo(PipelineJob, { foreignKey: 'job_id' });
 
 PipelineJob.hasOne(Stage2Data, { foreignKey: 'job_id', as: 'Stage2' });
 Stage2Data.belongsTo(PipelineJob, { foreignKey: 'job_id' });
@@ -57,7 +63,7 @@ Stage2Data.belongsTo(PipelineJob, { foreignKey: 'job_id' });
 PipelineJob.hasOne(Stage3Data, { foreignKey: 'job_id', as: 'Stage3' });
 Stage3Data.belongsTo(PipelineJob, { foreignKey: 'job_id' });
 
-// Stage3Container is directly associated with PipelineJob
+// CORRECTION: Stage3Container is directly associated with PipelineJob, not Stage3Data
 PipelineJob.hasMany(Stage3Container, { foreignKey: 'job_id', as: 'Stage3Containers' });
 Stage3Container.belongsTo(PipelineJob, { foreignKey: 'job_id' });
 
@@ -81,14 +87,7 @@ TaskUpdate.belongsTo(Task, { foreignKey: 'task_id' });
 User.hasMany(PipelineJob, { foreignKey: 'created_by', as: 'CreatedJobs' });
 PipelineJob.belongsTo(User, { foreignKey: 'created_by', as: 'CreatedByUser' });
 
-User.hasMany(PipelineJob, { foreignKey: 'assigned_to_stage2', as: 'Stage2Jobs' });
-PipelineJob.belongsTo(User, { foreignKey: 'assigned_to_stage2', as: 'Stage2User' });
 
-User.hasMany(PipelineJob, { foreignKey: 'assigned_to_stage3', as: 'Stage3Jobs' });
-PipelineJob.belongsTo(User, { foreignKey: 'assigned_to_stage3', as: 'Stage3User' });
-
-User.hasMany(PipelineJob, { foreignKey: 'customer_id', as: 'CustomerJobs' });
-PipelineJob.belongsTo(User, { foreignKey: 'customer_id', as: 'Customer' });
 
 User.hasMany(JobFile, { foreignKey: 'uploaded_by', as: 'UploadedFiles' });
 JobFile.belongsTo(User, { foreignKey: 'uploaded_by', as: 'UploadedByUser' });
@@ -130,6 +129,7 @@ module.exports = {
   User,
   PipelineJob,
   Stage1Data,
+  Stage1Container,
   Stage2Data,
   Stage3Data,
   Stage3Container,
